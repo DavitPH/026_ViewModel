9db2b51967487eebe07acd3986fbb4d994cffdc8
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-10-24 19:31:15 +0700

    add TampilLayout

--- app/src/main/java/com/example/a026_viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/a026_viewmodel/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.a026_viewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a026_viewmodel.ui.theme._026_ViewModelTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +35,27 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun TampilLaypout(modifier: Modifier = Modifier){
+    Card(modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ){
+        Column(
+            verticaArrangement = Arrangement.spacedBy(10.dp),
+            horizontalAlingment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(20.dp)
+        ){
+
+        }
+    }
+}
+
+@Composable
+fun TampilForm(){
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
